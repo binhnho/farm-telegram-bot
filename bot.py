371fa61369e383a80580148b6578ee05130c3ce4
binhnho <nguyenphuongbinh1609@gmail.com>
2025-03-03 02:06:12 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 from telegram import Update
 from telegram.ext import Application, CommandHandler, CallbackContext
 import os
+import asyncio
 
 # Lấy TOKEN từ biến môi trường
 TOKEN = os.getenv("TOKEN")
@@ -8,6 +9,13 @@ TOKEN = os.getenv("TOKEN")
 async def start(update: Update, context: CallbackContext):
     await update.message.reply_text("Chào mừng bạn đến với game nông trại!")
 
-app = Application.builder().token(TOKEN).build()
-app.add_handler(CommandHandler("start", start))
-app.run_polling()
+async def main():
+    app = Application.builder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    
+    print("Bot đang chạy...")
+    await app.run_polling()
+
+if __name__ == "__main__":
+    asyncio.run(main())
+    
